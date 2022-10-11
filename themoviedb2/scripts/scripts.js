b19fc372441560e35a479b5cd87482db93ea8a23
Carlos Baez Fumero <carlos.baez.fumero@gmail.com>
2022-10-11 16:14:08 +0200

    finally finished functionality, now adding some css

--- themoviedb2/scripts/scripts.js
+++ themoviedb2/scripts/scripts.js
@@ -1,28 +1,101 @@
 // API information
 baseUrl = "https://api.themoviedb.org/3/trending/all/week"
 apiKey = "?api_key=20fb29b2c9f122eaa6465a6caf734247"
-posterPath = "https://image.tmdb.org/t/p/w185"
+basePosterRoute = "https://image.tmdb.org/t/p/w185"
 
-// moviesJson contains the result of fetching themoviedb api data
-let moviesJson
-
-// get the UL id
-const trendingMoviesUl = document.getElementById("trending-movies-ul")
+const trendingMoviesUl = document.querySelector("ul");
 
 // Function to fetch popular movie data
 function fetchMovies() {
   fetch(`${baseUrl}${apiKey}`)
   .then((response) => response.json())
   // .then((data) => console.log(data));
-  .then((data) => moviesJson = data.results)
+  .then((data) => {
+    const moviesJson = data.results
+    moviesJson.forEach(movie => {
+      const listItem = document.createElement("li");
+
+      const titleElement = document.createElement("p");
+      if (movie.media_type == "movie") {
+        titleElement.textContent = movie.original_title;
+      } else {
+        titleElement.textContent = movie.name;
+      }
+
+      const imageElement = document.createElement("img");
+      const posterPathUrl = `${basePosterRoute}${movie.poster_path}`
+      imageElement.setAttribute("src", posterPathUrl);
+
+      listItem.append(
+      titleElement,
+      imageElement
+      );
+
+      const trendingMoviesUl = document.querySelector("ul");
+      trendingMoviesUl.appendChild(listItem);
+    });
+  });
 }
 
-function displayMovies(trendingMovie) {
-  let li = document.createElement("li")
-  li.appendChild(document.createTextNode(trendingMovie))
-  trendingMoviesUl.appendChild(li)
-}
+fetchMovies();
 
-fetchMovies()
-console.log(moviesJson)
-displayMovies()
+// function displayMovies(moviesJson) {
+  // for (const movie in movies) {
+  //   console.log(movie)
+  //   const listItem = document.createElement("li");
+
+  //   const titleElement = document.createElement("strong");
+  //   titleElement.textContent = movie.original_title;
+
+  //   // const imageElement = document.createElement("img");
+  //   // const posterPathUrl = `${basePosterRoute}${movie.poster_path}`
+  //   // img.setAttribute("src", posterPathUrl);
+
+  //   listItem.append(
+  //     titleElement
+  //     // imageElement
+  //   );
+  //   trendingMoviesUl.appendChild(listItem);
+  // }
+
+//   console.log(moviesJson)
+//   moviesJson.forEach(movie => {
+//     console.log(movie)
+//   });
+// }
+
+// displayMovies(moviesJson);
+// displayMovies(moviesJson.at(0).original_title)
+
+// const myList = document.querySelector("ul");
+
+// fetch("products.json")
+//   .then((response) => {
+//     if (!response.ok) {
+//       throw new Error(`HTTP error, status = ${response.status}`);
+//     }
+//     return response.json();
+//   })
+//   .then((data) => {
+//     for (const product of data.products) {
+//       const listItem = document.createElement("li");
+
+//       const nameElement = document.createElement("strong");
+//       nameElement.textContent = product.Name;
+
+//       const priceElement = document.createElement("strong");
+//       priceElement.textContent = `£${product.Price}`;
+
+//       listItem.append(
+//         nameElement,
+//         ` can be found in ${product.Location}. Cost: `,
+//         priceElement,
+//       );
+//       myList.appendChild(listItem);
+//     }
+//   })
+//   .catch((error) => {
+//     const p = document.createElement("p");
+//     p.appendChild(document.createTextNode(`Error: ${error.message}`));
+//     document.body.insertBefore(p, myList);
+//   });
